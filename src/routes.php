9d7cde93c7c13df2981bac578a5c1924b1f60feb
Jonas Nielsen <dev@jonas92.com>
2016-07-25 19:19:34 +0200

    Changed routes and template locations

--- src/routes.php
+++ src/routes.php
@@ -1,40 +1,22 @@
 <?php
+
 // Routes
-
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
-});
-
-$app->get('/hello/[{name}]', function ($request, $response, $args) {
-	// Log
-	$this->logger->info("Hello World! '/hello/' route");
-
-	// Render view
-	return $this->twig->render('index.html', [
-        'name' => $args['name']
-	]);
-});
-
-$app->get('/webgl/test', function ($request, $response, $args) use ($app) {
+$app->get('/', function ($request, $response, $args) use ($app) {
 	// Log 
-	$this->logger->info("WebGL test '/webgl/test' route");
+	$this->logger->info("Asteroids '/' route");
 	
 	// Render view
-	return $this->twig->render('webgl/test.html', [
+	return $this->twig->render('index.html', [
 		'website' => $app->website
 	]);
 });
 
-$app->get('/webgl/test/fullscreen', function ($request, $response, $args) use ($app) {
+$app->get('/src', function ($request, $response, $args) use ($app) {
 	// Log
-	$this->logger->info("WebGL test game '/webgl/test/fullscreen' route");
+	$this->logger->info("Asteroids src '/src' route");
 
 	// Render view
-	return $this->twig->render('webgl/test/game.html', [
+	return $this->twig->render('game.html', [
         'website' => $app->website,
         'resource' => $app->resource
 	]);
